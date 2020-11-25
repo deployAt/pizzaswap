6ba0d3e180c5c13ca784651036b835246eaedf36
deployAt <mikowhy@tutanota.com>
2020-11-25 20:26:17 +0100

    feat: configure truffle

--- truffle-config.js
+++ truffle-config.js
@@ -1,23 +1,33 @@
+require('dotenv').config()
+const HDWalletProvider = require('truffle-hdwallet-provider-privkey')
+const privateKeys = process.env.PRIVATE_KEYS || ''
+
 module.exports = {
   networks: {
     development: {
-      host: '127.0.0.1', // Localhost (default: none)
-      port: 8545, // Standard Ethereum port (default: none)
-      network_id: '*', // Any network (default: none)
+      host: '127.0.0.1',
+      port: 7545,
+      network_id: '*',
+    },
+    rinkeby: {
+      provider: function () {
+        return new HDWalletProvider(
+          privateKeys.split(','),
+          `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`
+        )
+      },
+      gas: 5000000,
+      gasPrice: 25000000000,
+      network_id: 4,
     },
   },
-
+  plugins: ['truffle-plugin-verify'],
+  api_keys: {
+    etherscan: process.env.ETHERSCAN_API_KEY,
+  },
   compilers: {
     solc: {
-      // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
+      version: '0.6.12',
     },
   },
 }
